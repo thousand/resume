331f196af05d615feb00c42184e487f7191c00fb
Bryan Levay <bryan@bryanlevay.com>
2014-02-15 12:56:10 -0600

    Some content and style

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,18 @@
  * GET home page.
  */
 
+var path = require('path')
+  , work = require( path.join( __dirname, '../work.json' ) )
+;
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  console.log(work);
+  res.render(
+    'index',
+    {
+      title: 'Resume',
+      name: 'Bryan Levay',
+      work: work
+    }
+  );
 };
